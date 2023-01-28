c43bf918f453aaf8a2d7ab16c7bbbc8a269ad5f2
Ben L <veloswim@gmail.com>
2023-01-28 10:19:20 -0800

    added routes in apiRoutes

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const util = require("util");
+const fs = require("fs");
+const promWrite = util.promisify(fs.writeFile);
+const promRead = util.promisify(fs.readFile);
 
-router.get("/notes", (req, res) => res.send("you dun it look at the url"));
+router.get("/notes", (req, res) =>
+  res.send("this is where we need to send notes back to js, see line 28")
+);
+
+router.post("/notes", (req, res) => {
+  res.send(
+    "need to write to ../db.json here, check the req.body when receiving."
+  );
+});
+
+router.delete("/notes/:id", (req, res) => {
+  res.send(
+    "depending on id of notes, we need to loop through array of objects in ../db.json to remove"
+  );
+});
 
 module.exports = router;
