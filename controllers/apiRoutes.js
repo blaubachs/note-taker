118c26d40ce9a91a903afd0fef593632ca553ede
Ben L <veloswim@gmail.com>
2023-01-28 22:35:28 -0800

    added route to save notes

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -10,7 +10,23 @@ router.get("/notes", (req, res) =>
   res.send("this is where we need to send notes back to js, see line 28")
 );
 
-router.post("/notes", (req, res) => {
+router.post("/notes", async (req, res) => {
+  const noteReceived = req.body;
+  const notes = await promRead("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      res.send("err in reading file");
+      throw err;
+    } else {
+      console.log(data);
+      let newArr = JSON.parse(data);
+      newArr.push(req.body);
+      promWrite("./db/db.json", JSON.stringify(newArr, null, 4), (err) =>
+        err ? console.log(err) : res.send("updated")
+      );
+      return;
+    }
+  });
+
   res.send(
     "need to write to ../db.json here, check the req.body when receiving."
   );
