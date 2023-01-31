a8cb89caf23bd0af5005b4535f9d5ae38b34a180
Ben L <veloswim@gmail.com>
2023-01-30 19:17:20 -0800

    removed console logs

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -14,7 +14,6 @@ router.get("/notes", (req, res) => {
 router.post("/notes", async (req, res) => {
   const { title, text } = req.body;
   let id = uuidv4();
-  console.log(title, text);
   const newNoteObj = {
     id: id,
     title: title,
@@ -25,7 +24,6 @@ router.post("/notes", async (req, res) => {
       res.send("err in reading file");
       throw err;
     } else {
-      console.log(data);
       let newArr = JSON.parse(data);
       newArr.push(newNoteObj);
       promWrite("./db/db.json", JSON.stringify(newArr, null, 4), (err) =>
